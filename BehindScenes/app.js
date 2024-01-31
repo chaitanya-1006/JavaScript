3bdd9bf44670ea2991490622636e081983658615
chaitanya-1006 <manojchaitanya99@gmail.com>
2024-01-31 06:21:20 +0530

    behind the scenes of javascript

---
+++ BehindScenes/app.js
@@ -0,0 +1,19 @@
+var name ='Manoj';
+
+if (name === 'Manoj')
+{
+    let hobbies=['Sports','Cooking'];
+    console.log(hobbies);
+}
+
+function greet()
+{
+    var age=24;
+    var name='Chaitanya'
+    console.log(name,age);
+}
+
+greet();
+
+console.log(name,hobbies);
+
